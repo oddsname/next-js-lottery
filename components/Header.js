f8c89fef5f0121c914e587d92a641be91593c1ea
oddsname <vadimich2012@mail.ru>
2023-06-06 11:10:16 +0300

    added connect button

--- components/Header.js
+++ components/Header.js
@@ -2,21 +2,34 @@ import {useMoralis} from "react-moralis";
 import {useEffect} from "react";
 
 export default function Header() {
-    const {enableWeb3, isWeb3Enabled, account} = useMoralis();
+    const {enableWeb3, isWeb3Enabled, account, Moralis, deactivateWeb3, isWeb3EnableLoading} = useMoralis();
 
     useEffect(() => {
-        console.log('hie');
+        if(!isWeb3Enabled && window.localStorage.getItem('connected')) {
+            enableWeb3().then(() => {});
+        }
     }, [isWeb3Enabled]);
+
+    useEffect(() => {
+        Moralis.onAccountChanged((account) => {
+            console.log(`account change to ${account}`);
+            if(account === null) {
+                window.localStorage.removeItem('connected');
+                deactivateWeb3().then(() => {});
+            }
+        })
+    }, []);
 
     const connect = async () => {
         await enableWeb3();
+        window.localStorage.setItem('connected', 'true');
     }
 
     return (
         <div>
             {account
                 ? <div> Connected to {account}</div>
-                : <button onClick={connect}>Connect</button>
+                : <button onClick={connect} disabled={isWeb3EnableLoading}>Connect</button>
             }
         </div>
     );
