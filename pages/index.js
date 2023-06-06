e7cc2214c13de12a197869f1d4fb8cdb5747cf3d
oddsname <vadimich2012@mail.ru>
2023-06-06 11:22:37 +0300

    added web3uikit package, added new header

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
+import ManualHeader from "@/components/ManualHeader";
 import Header from "@/components/Header";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
+            {/*<ManualHeader />*/}
             <Header />
         </div>
     )
